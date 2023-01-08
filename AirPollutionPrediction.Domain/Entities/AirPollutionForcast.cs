25761fde38e783709cfc8d16fa9f2b7ca89fb484
guna.ashok1991 <guna.ashok1991@hotmail.com>
2023-01-08 08:27:19 +0530

    #Update

---
+++ AirPollutionPrediction.Domain/Entities/AirPollutionForcast.cs
@@ -0,0 +1,17 @@
+﻿using AirPollutionPrediction.Domain.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirPollutionPrediction.Domain.Entities
+{
+    public class AirPollutionForcast:AuditableEntity<int>
+    {
+        public string CO { get; set; }
+        public string NO2 { get; set; }
+        public string SO2 { get; set; }
+        public int Horizon { get; set; }
+    }
+}
